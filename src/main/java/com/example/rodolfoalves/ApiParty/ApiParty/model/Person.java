9b3f392d181f2f3f39f50795e9bd091a4da7dc5d
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-08-19 17:35:21 -0300

    feat(Person): add attributes

---
+++ src/main/java/com/example/rodolfoalves/ApiParty/ApiParty/model/Person.java
@@ -0,0 +1,26 @@
+package com.example.rodolfoalves.ApiParty.ApiParty.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private int age;
+}
