a1254c4bbce39926e1e4ef0c210bbd124e64ce97
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-08-22 17:05:57 -0300

    feat(Person): add jakarta import

--- src/main/java/com/example/rodolfoalves/ApiParty/ApiParty/model/Person.java
+++ src/main/java/com/example/rodolfoalves/ApiParty/ApiParty/model/Person.java
@@ -1,6 +1,7 @@
 package com.example.rodolfoalves.ApiParty.ApiParty.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
