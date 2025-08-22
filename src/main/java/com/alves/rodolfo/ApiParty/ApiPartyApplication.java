b14ad0dd1dc7ad0332c3dbbc5db0ad62d056c88c
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-08-22 17:09:51 -0300

    feat(app): correct folder's name

--- src/main/java/com/alves/rodolfo/ApiParty/ApiPartyApplication.java
+++ src/main/java/com/alves/rodolfo/ApiParty/ApiPartyApplication.java
@@ -1,4 +1,4 @@
-package com.example.rodolfoalves.ApiParty.ApiParty;
+package com.alves.rodolfo.ApiParty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/alves/rodolfo/ApiParty/model/Party.java
+++ src/main/java/com/alves/rodolfo/ApiParty/model/Party.java
@@ -1,4 +1,4 @@
-package com.example.rodolfoalves.ApiParty.ApiParty.model;
+package com.alves.rodolfo.ApiParty.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
