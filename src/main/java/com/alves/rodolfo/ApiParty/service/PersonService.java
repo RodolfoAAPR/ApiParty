5578f0f2496a54d737ac321d440fe875740f60b5
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-08-25 17:37:50 -0300

    feat(Person): add party repository on class

--- src/main/java/com/alves/rodolfo/ApiParty/service/PersonService.java
+++ src/main/java/com/alves/rodolfo/ApiParty/service/PersonService.java
@@ -1,6 +1,7 @@
 package com.alves.rodolfo.ApiParty.service;
 
 import com.alves.rodolfo.ApiParty.model.Person;
+import com.alves.rodolfo.ApiParty.repository.PartyRepository;
 import com.alves.rodolfo.ApiParty.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,14 +12,13 @@ public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
+    @Autowired
+    private PartyRepository partyRepository;
+
     public void createPerson(Person person){
         personRepository.save(person);
     }
 
-    public Person findPersonById(Long id){
-        return personRepository.findById(id)
-                .orElse(null);
     }
-}
 
 
