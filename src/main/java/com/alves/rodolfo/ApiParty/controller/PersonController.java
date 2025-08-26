521c372d1473ecc7ebb3486bb08d0abb8afad696
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-08-26 17:24:23 -0300

    refactor(PersonController): correct function register person and add minimum age

--- src/main/java/com/alves/rodolfo/ApiParty/controller/PersonController.java
+++ src/main/java/com/alves/rodolfo/ApiParty/controller/PersonController.java
@@ -1,8 +1,10 @@
 package com.alves.rodolfo.ApiParty.controller;
 
 import com.alves.rodolfo.ApiParty.model.Person;
+import com.alves.rodolfo.ApiParty.repository.PersonRepository;
 import com.alves.rodolfo.ApiParty.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,12 +14,17 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    public void registerPerson(@RequestBody Person person){
-        personService.createPerson(person);
-    }
+    @Autowired
+    private PersonRepository personRepository;
 
-    @PostMapping("/{id}/add/{idPerson}")
-    public void addPersonToParty(@PathVariable Long id, @PathVariable Long idPerson) {
-        personService.addPersonToParty(id, idPerson);
+
+    @PostMapping("/person")
+    public ResponseEntity<String> registerPerson(@RequestBody Person person) {
+        if(person.getAge() < 18){
+            return ResponseEntity.badRequest().body(person.getName() + " must have 18 years old or more.");
+        }
+
+        personRepository.save(person);
+        return ResponseEntity.ok(person.getName()+ " registered successfully.");
     }
 }
