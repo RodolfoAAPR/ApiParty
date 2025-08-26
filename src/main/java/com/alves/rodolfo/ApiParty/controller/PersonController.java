dcc56c3847f4b164e24c8f8de67838b4ff973d94
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-08-26 17:37:06 -0300

    refactor(PersonController): rename get route

--- src/main/java/com/alves/rodolfo/ApiParty/controller/PersonController.java
+++ src/main/java/com/alves/rodolfo/ApiParty/controller/PersonController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/guests")
 public class PersonController {
 
     @Autowired
