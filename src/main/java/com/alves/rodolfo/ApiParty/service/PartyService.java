5baf079f1bf3da73b64406bb6ff8526a40b32aa7
RodolfoAAPR <165041838+RodolfoAAPR@users.noreply.github.com>
2025-09-10 16:16:40 -0300

    refactor(PartyService): remove unused imports

--- src/main/java/com/alves/rodolfo/ApiParty/service/PartyService.java
+++ src/main/java/com/alves/rodolfo/ApiParty/service/PartyService.java
@@ -7,8 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Service
 public class PartyService {
