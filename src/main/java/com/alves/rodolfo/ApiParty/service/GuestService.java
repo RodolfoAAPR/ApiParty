742e7306fcb1929e656701ebd85f21cafd5e7e36
RodolfoAAPR <165041838+RodolfoAAPR@users.noreply.github.com>
2025-09-11 18:01:31 -0300

    refactor(GuestService): correct method create guest and find by id

--- src/main/java/com/alves/rodolfo/ApiParty/service/GuestService.java
+++ src/main/java/com/alves/rodolfo/ApiParty/service/GuestService.java
@@ -4,7 +4,9 @@ import com.alves.rodolfo.ApiParty.model.Guest;
 import com.alves.rodolfo.ApiParty.repository.PartyRepository;
 import com.alves.rodolfo.ApiParty.repository.GuestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Optional;
 
@@ -18,14 +20,17 @@ public class GuestService {
     private PartyRepository partyRepository;
 
     public Guest findById(Long id){
-        Optional<Guest>  person = this.personRepository.findById(id);
-        return person.orElseThrow(() -> new RuntimeException(
+        Optional<Guest>  guest = this.personRepository.findById(id);
+        return guest.orElseThrow(() -> new RuntimeException(
                 "Usuário " + Guest.class.getName() + " não encontrado."
         ));
     }
 
-    public Guest createPerson(Guest person){
-        return personRepository.save(person);
+    public Guest createPerson(Guest guest){
+        if(guest.getAge() < 18)
+            ResponseEntity.badRequest().body(guest.getName() + " must have 18 years old at least!");
+
+        return personRepository.save(guest);
     }
 
 }
