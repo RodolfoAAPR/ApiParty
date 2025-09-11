e105c2eb39affb3ad8b62c85efa5e04f79f46bc4
RodolfoAAPR <165041838+RodolfoAAPR@users.noreply.github.com>
2025-09-11 17:47:36 -0300

    refactor(Guest): correct class name and parameters

--- src/main/java/com/alves/rodolfo/ApiParty/controller/GuestController.java
+++ src/main/java/com/alves/rodolfo/ApiParty/controller/GuestController.java
@@ -2,7 +2,7 @@ package com.alves.rodolfo.ApiParty.controller;
 
 import com.alves.rodolfo.ApiParty.model.Guest;
 import com.alves.rodolfo.ApiParty.repository.GuestRepository;
-import com.alves.rodolfo.ApiParty.service.PersonService;
+import com.alves.rodolfo.ApiParty.service.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +14,7 @@ import java.util.List;
 public class GuestController {
 
     @Autowired
-    private PersonService personService;
+    private GuestService personService;
 
     @Autowired
     private GuestRepository personRepository;
--- src/main/java/com/alves/rodolfo/ApiParty/service/GuestService.java
+++ src/main/java/com/alves/rodolfo/ApiParty/service/GuestService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class PersonService {
+public class GuestService {
 
     @Autowired
     private GuestRepository personRepository;
